52ae993de4c88fbe76ef49849a59679ed737a2e4
shreyesh98 <shreyesh98@gmail.com>
2020-12-20 13:43:36 +0530

    sbs v2

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+// var name = prompt("What's your name??")
+// var welcome = alert("Welcome to Sai Bhargavi Residency "+ name)
+
+$(document).ready(function(){
+
+// scrolling effect
+
+ $("a").on('click', function(event) {
+    if (this.hash !== "") {
+       event.preventDefault();
+       var hash = this.hash;
+      $('html, body').animate({scrollTop: $(hash).offset().top}, 1000, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+
+// navbar scroll change
+
+ $(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar.fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+
+});
